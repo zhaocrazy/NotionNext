7e5e76ffd7dd0a759adfc7813dc315f98ac023a7
xiao <xiao1024@MacBook-Pro.local>
2025-10-30 00:41:39 +0800

    optimz shoptofly png

--- themes/shoptofly/config.js
+++ themes/shoptofly/config.js
@@ -21,7 +21,7 @@ const CONFIG = {
   STARTER_HERO_BUTTON_2_ICON: '/images/shoptofly/Bilibili_logo.svg', // 英雄区按钮2的图标，不需要则留空
 
   // 英雄区配图，如需隐藏，改为空值即可 ''
-  STARTER_HERO_PREVIEW_IMAGE: '/images/shoptofly/hero/shoptofly1.png', // 产品预览图 ，默认读取public目录下图片
+  STARTER_HERO_PREVIEW_IMAGE: '/images/shoptofly/hero/shoptofly.png', // 产品预览图 ，默认读取public目录下图片
   STARTER_HERO_BANNER_IMAGE: '', // hero区下方的全宽图
 
   SHOPTOFLY_TOP_WORD1: '🚀 11月-双11-免费技术咨询',   //优惠框1
